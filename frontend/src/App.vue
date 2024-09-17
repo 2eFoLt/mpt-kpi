21c9522b6ad689a0130a6d1a53384e1c47144189
Kirill Vasylyev <vasylyevkirill@yandex.com>
2024-09-17 04:18:40 +0300

    compose initial commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <h1>Здесь когда-то будет frontend.</h1>
+  <p>
+    А пока ты можешь проверить как круто проксируется
+    <a href="/api">backend</a>
+    !
+  </p>
 </template>
 
 <script>
