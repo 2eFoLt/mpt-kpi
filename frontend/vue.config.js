212f13d1f4a5313778fc38e0df19723b944639b3
Kirill Vasylyev <vasylyevkirill@yandex.com>
2024-09-17 03:30:07 +0300

    frontned initial commit

---
+++ frontend/vue.config.js
@@ -0,0 +1,32 @@
+const { defineConfig } = require("@vue/cli-service");
+
+const HOST = "0.0.0.0";
+const FRONTEND_PORT = process.env.FRONTEND_PORT;
+const BACKEND_PORT = process.env.BACKEND_PORT;
+const BACKEND_PATH = `http://backend:${BACKEND_PORT}/`;
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    port: FRONTEND_PORT,
+    host: HOST,
+    proxy: {
+      "/api": {
+        target: BACKEND_PATH + "/api",
+        secure: false,
+        changeOrigin: true,
+        prependPath: true,
+      },
+      "/media": {
+        target: BACKEND_PATH + "/media",
+        changeOrigin: true,
+        prependPath: true,
+      },
+      "/static": {
+        target: BACKEND_PATH + "/static",
+        changeOrigin: true,
+        prependPath: true,
+      },
+    },
+  },
+});
