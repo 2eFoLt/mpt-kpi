bfb837c5868c93e60060dcf0b6e7cfc83552a6a7
Кирилл Васильев <vasylyev@artlebedev.ru>
2024-11-25 04:51:08 +0300

    fix login page styles

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -5,90 +5,111 @@ export default {
 </script>
 
 <template>
-  <div id="page">
+  <div class="auth-container">
     <h1>ЦГБ им А.С. Пушкина г. Орёл</h1>
-    <div id="authorize">
+    <div class="popup-wrapper">
       <h2>Вход</h2>
-      <form>
-        <input
-          type="text"
-          class="text-input"
-          name="login"
-          placeholder="Логин"
-        />
-        <input
-          type="password"
-          class="text-input"
-          name="password"
-          placeholder="Пароль"
-        />
+      <form class="auth-form">
+        <div class="input-container">
+          <input
+            type="text"
+            class="text-input"
+            name="login"
+            placeholder="Логин"
+          />
+          <input
+            type="password"
+            class="text-input"
+            name="password"
+            placeholder="Пароль"
+          />
+        </div>
         <input type="submit" class="submit-input" name="signin" value="Войти" />
       </form>
     </div>
   </div>
 </template>
 
-<style>
-#authorize {
-  width: 22rem;
-  height: 18rem;
-  background-color: white;
-  border-radius: 0.6em;
+<style lang="sass" scoped>
+.popup-wrapper {
+    width: 100%;
+	max-width: 26em;
+    padding: 3.75em 2em;
+    text-align: center;
+
+	background-color: white;
+	border-radius: 0.6em;
 }
 
-#page {
-  background-color: #5076b6;
+.auth-form {
+    width: 100%;
+    padding-top: 4em;
+}
+
+.auth-container {
+	height: 100vh;
+	background-color: #5076b6;
 }
 
 form {
-  justify-content: space-between;
+	justify-content: space-between;
 }
 
-#page,
-#authorize,
+.input-container {
+    width: 100%;
+    gap: 1em;
+    padding-bottom: 6em;
+}
+
+.popup-wrapper,
+.auth-container,
+.input-container,
 form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 }
 
 h1 {
-  color: white;
-  font-size: 1.6em;
-  font-weight: 600;
-  line-height: 2em;
-  /*font-family: ; */
+	color: white;
+	font-size: 1.6em;
+	font-weight: 600;
+	color: white;
+	line-height: 2em;
+	/*font-family: ; */
 }
 
 h2 {
-  color: #5076b6;
-  font-size: 1.4em;
-  font-weight: 600;
-  line-height: 2em;
-  /*font-family: ; */
+	color: #5076b6;
+	font-size: 1.6em;
+	font-weight: 600;
+	line-height: 2em;
+	/*font-family: ; */
 }
 
 .text-input {
-  width: 19em;
-  /*font-family: ; */
-  font-size: 1em;
-  font-weight: 500;
-  line-height: 2em;
-  padding-left: 1.2em;
-  text-align: left;
-  background-color: #ccdef1;
-  border-radius: 0.5em;
-  border-style: hidden;
-  background-clip: padding-box;
-  outline: none;
+	width: 100%;
+	/*font-family: ; */
+	font-size: 1em;
+	font-weight: 500;
+	line-height: 2.5em;
+	padding-left: 1.2em;
+	text-align: left;
+	background-color: #ccdef1;
+	border-radius: 0.5em;
+	border-style: hidden;
+	background-clip: padding-box;
+	outline: none;
 }
 
 .submit-input {
-  color: white;
-  width: 8em;
-  height: 2.2em;
-  background-color: #5076b6;
-  border-radius: 0.4em;
-  border-style: hidden;
+	color: white;
+	width: 10em;
+	height: 3em;
+	font-size: 1em;
+	font-weight: 600;
+	background-color: #5076b6;
+	border-radius: 0.4em;
+	border-style: hidden;
 }
 </style>
