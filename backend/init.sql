58f6ed2f26a52302dd81ffdaf7b5477be8e935c3
Esferaty <mcdiban@yahoo.com>
2024-11-28 00:58:31 +0300

    1) дополнил БД 2) добавлина авторизация

--- backend/init.sql
+++ backend/init.sql
@@ -5,15 +5,24 @@ USE mpt-kpi;
 CREATE TABLE IF NOT EXISTS Role (
     Role_ID INT AUTO_INCREMENT PRIMARY KEY,
     Role_name VARCHAR(45) NOT NULL,
+    Role_points INT NOT NULL,
     Role_description VARCHAR(125)
 );
 
 -- Creating the Users table
 CREATE TABLE IF NOT EXISTS Users (
     User_ID INT AUTO_INCREMENT PRIMARY KEY,
+    User_name VARCHAR(45) NOT NULL,
+    User_surname VARCHAR(45) NOT NULL,
+    User_patronymic VARCHAR(45) NOT NULL,
+    User_email VARCHAR(45) NOT NULL,
+    User_phone INT NOT NULL,
+    User_passhash VARCHAR(100) NOT NULL,
+    User_role VARCHAR(100) NOT NULL,
     User_login VARCHAR(45) NOT NULL,
     User_password VARCHAR(100) NOT NULL,
     User_hash VARCHAR(100),
+    is_admin BOOLEAN NOT NULL,
     Role_Role_ID INT,
     FOREIGN KEY (Role_Role_ID) REFERENCES Role(Role_ID)
 );
