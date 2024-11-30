555a719c7fcaf34c5c7b80ca469c9f44fcc19cfe
Кирилл Васильев <vasylyev@artlebedev.ru>
2024-11-30 12:52:58 +0300

    frontend add popups

--- frontend/src/pages/ProfilePage.vue
+++ frontend/src/pages/ProfilePage.vue
@@ -4,23 +4,33 @@
     <div class="details">
       <ProfileDetails />
     </div>
+    <AddCertificatePopup
+      v-if="showAddCertificatePopup"
+      v-model="showAddCertificatePopup"
+    />
   </div>
 </template>
 
 <script>
 import ProfileMenu from "@/components/Profile/ProfileMenu.vue";
 import ProfileDetails from "@/components/Profile/ProfileDetails.vue";
+import AddCertificatePopup from "@/components/Profile/AddCertificatePopup.vue";
 
 export default {
+  data() {
+    return {
+      showAddCertificatePopup: true,
+    };
+  },
   components: {
     ProfileMenu,
     ProfileDetails,
+    AddCertificatePopup,
   },
 };
 </script>
 
 <style lang="sass" scoped>
-
 #main {
 	min-height: 100vh;
 	background: #5076B6;
